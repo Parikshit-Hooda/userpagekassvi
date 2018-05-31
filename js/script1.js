c401627fdde5ebed2a8d8f9289d328f0bf80878f
Parikshit Hooda <phooda804@live.com>
2018-05-31 11:10:32 +0530

    uncomment logs for debug

--- js/script1.js
+++ js/script1.js
@@ -20,28 +20,28 @@ var recorder;
 
 function startUserMedia(stream) {
     var input = audio_context.createMediaStreamSource(stream);
-    // __log('Media stream created.');
+    __log('Media stream created.');
 
     // Uncomment if you want the audio to feedback directly
     //input.connect(audio_context.destination);
     //__log('Input connected to audio context destination.');
 
     recorder = new Recorder(input);
-    // __log('Recorder initialised.');
+    __log('Recorder initialised.');
 }
 
 function startRecording(button) {
     recorder && recorder.record();
     button.disabled = true;
     button.nextElementSibling.disabled = false;
-    // __log('Recording...');
+    __log('Recording...');
 }
 
 function stopRecording(button) {
     recorder && recorder.stop();
     button.disabled = true;
     button.previousElementSibling.disabled = false;
-    // __log('Stopped recording.');
+    __log('Stopped recording.');
 
     // create WAV download link using audio data blob
     createDownloadLink();
@@ -122,8 +122,8 @@ window.onload = function init() {
         window.URL = window.URL || window.webkitURL;
 
         audio_context = new AudioContext;
-        // __log('Audio context set up.');
-        // __log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
+        __log('Audio context set up.');
+        __log('navigator.getUserMedia ' + (navigator.getUserMedia ? 'available.' : 'not present!'));
     } catch (e) {
         alert('No web audio support in this browser!');
     }
